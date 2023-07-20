a6b704700df6f1213a98f6473643e62092b3ad53
smsohelrana44 <smsohelrana774@gmail.com>
2023-07-20 20:01:44 +0600

    Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,43 @@
 /*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu'),
+ navToggle=document.getElementById('nav-toggle'),
+ navClose=document.getElementById('nav-close')
 
+ /*========= MENU SHOW ======= */
+ /* Validate if constant exists */
+
+ if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu')
+    })
+ }
+
+ /* ======= MENU HIDDEN ======== */
+ /* Validate if constant exists */
+ if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+ }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink=document.querySelectorAll('.nav_link')
+
+const linkAction=()=>{
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 /*=============== ADD BLUR TO HEADER ===============*/
+ const blurHeader =() =>{
+    const header=document.getElementById('header')
+    this.scrolly>= 50 ? header.classList.add('blur-header')
+                  : header.classList.remove('blur-header')
+ }
+ window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
